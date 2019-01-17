d1728d85a0063aa0fe5d3e128d1f0072db048949
Brian Blosser <bpb54321@gmail.com>
2019-01-17 09:48:39 -0500

    Grab options from ldap.ini only if a bind username was entered in the options.

--- CentralAuthPlugin.php
+++ CentralAuthPlugin.php
@@ -227,8 +227,8 @@ class CentralAuthPlugin extends Omeka_Plugin_AbstractPlugin
             }
 
             // Get LDAP bind password from the ini file if configuration
-            // is set up to do this
-            if ($options['use_ini']) {
+            // is set up to do this and a bind username has been entered
+            if (isset($options['username']) && $options['use_ini']) {
                 $ldap_ini = parse_ini_file ('ldap.ini', true);
                 if (isset($ldap_ini['ldap']['bind_password'])) {
                     $options['password'] = $ldap_ini['ldap']['bind_password'];
@@ -238,7 +238,9 @@ class CentralAuthPlugin extends Omeka_Plugin_AbstractPlugin
             // Remove $options['use_ini'] because it's not an option that
             // is recognized by Zend_Ldap, it's only use is to tell us
             // where to find the 'password' option
-            unset($options['use_ini']);
+            if (isset($options['use_ini'])) {
+                unset($options['use_ini']);
+            }
 
             // Create new auth adapter with the options, username and password.
             $adapterLdap = new CentralAuth_LdapAdapter(
